9cb1d0a49bd2bebdb331cdccf271c7cc9a4a255c
malekcoders <149315149+malekcoders@users.noreply.github.com>
2024-07-02 14:31:55 +0300

    add menu

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 
 public class Main {
 
     static Display display = new Display();
+    
+    static Stack stack = new Stack();
 
     public static void main(String[] args) throws IOException {
 
-        Stack stack = new Stack();
 
         // function read file pasing the file as string 
         BufferedReader reader = new BufferedReader(new FileReader("history.txt"));
@@ -22,12 +25,24 @@ public class Main {
         
         reader.close();
 
-        // add url sites 
-        display.addSite("https://iabdulmalik.com");
+        while(true) {
+
+            // input dialog
+            String choice = JOptionPane.showInputDialog("1. Add site\n2. Navi\n3. Exit");
+            break;
+        }
+
         
+        // add site
+        add_site("https://safcsp.org.sa");
+
         // display url history
         while(stack.peek() != null){
             System.out.println(stack.pop());
         }
+    }
+
+    public static void add_site(String url){
+        stack.push(url);
     }
 }
