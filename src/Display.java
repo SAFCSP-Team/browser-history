8165de1d4b724442e611e1df281e4761368417f7
malekcoders <149315149+malekcoders@users.noreply.github.com>
2024-07-03 14:32:33 +0300

    complete implementation

--- src/Display.java
+++ src/Display.java
@@ -1,14 +1,59 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Display {
 
     // inject stack
-    private Stack stack;
+    private static Stack stack = new Stack();
+    
+    private static Scanner scanner = new Scanner(System.in);
 
-    public void display() {
-        
+    public void display() throws IOException {
+        while(true) {
+
+            // input dialog
+            // String choice = JOptionPane.showInputDialog("1.New site\n2. Previous site\n3. Exit");
+            
+            System.out.println("_________________________\n1-> New site URL\n2-> Previous site URL\n3-> Exit");
+            String choice = scanner.nextLine();
+
+            if(choice.equals("1")) {
+                System.out.print("Enter site URL: ");
+                String url = scanner.nextLine();
+                System.out.println();
+                add_site(url);
+            } else if(choice.equals("2")) {
+
+                System.out.println("Previous site: "+ stack.pop());
+                System.out.println();
+                
+            } else if(choice.equals("3")) {
+                System.out.println("Exiting...");
+                scanner.close();
+                break;
+            } else {
+                // JOptionPane.showMessageDialog(null, "Invalid choice");
+                System.out.println("Invalid choice");
+            }
+        }  
     }
 
-    public void addSite(String siteUrl) {
-        stack.push(siteUrl);
+    public void readFile() throws IOException{
+        // function read file pasing the file as string 
+        BufferedReader reader = new BufferedReader(new FileReader("history.txt"));
+        String line;
+         
+        while ((line = reader.readLine()) != null) {
+            
+            stack.push(line);
+        }
+        reader.close();
+    }
 
+    public static void add_site(String url){
+        
+        stack.push(url);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-
-import javax.swing.JOptionPane;
-
 
 public class Main {
 
@@ -13,36 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-
-        // function read file pasing the file as string 
-        BufferedReader reader = new BufferedReader(new FileReader("history.txt"));
-        String line;
-         
-        while ((line = reader.readLine()) != null) {
-            
-            stack.push(line);
-        }
-        
-        reader.close();
-
-        while(true) {
-
-            // input dialog
-            String choice = JOptionPane.showInputDialog("1. Add site\n2. Navi\n3. Exit");
-            break;
-        }
-
-        
-        // add site
-        add_site("https://safcsp.org.sa");
-
-        // display url history
-        while(stack.peek() != null){
-            System.out.println(stack.pop());
-        }
-    }
-
-    public static void add_site(String url){
-        stack.push(url);
+        display.readFile();
+        display.display();
     }
 }
